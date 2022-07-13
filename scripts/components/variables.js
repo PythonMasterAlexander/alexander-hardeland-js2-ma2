95d3a565de2af9cb1e04ad661451598a64b06f99
alexanderhardeland <alexanderhardeland@gmail.com>
2022-07-13 18:45:32 +0200

    Started to work on JS and process one

---
+++ scripts/components/variables.js
@@ -0,0 +1,20 @@
+export const listPageListContainer = document.querySelector('.list-page-list-container');
+
+export const books = [
+    {
+        isbn: "1600506460320",
+        title: "Great book",
+    },
+    {
+        isbn: "1600506460373",
+        title: "Ok book",
+    },
+    {
+        isbn: "1600506460521",
+        title: "Bad book",
+    },
+    {
+        isbn: "1600506460456",
+        title: "Terrible book",
+    },
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
-//Create a new file and write this code there
+import { books, listPageListContainer } from './components/variables.js';
+
+for(let i = 0; i < books.length; i++) {
+  const isbnBookNumber = books[i].isbn;
+  const bookTitle = books[i].title;
+
+
+}
+
 //TODO Display list users on page
 //TODO Remove individual list item using filter
 //TODO When list is empty display a new message 
